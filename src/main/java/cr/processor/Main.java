039e2871d2f67cb0ee6df1f16a019e8363ed118c
Fritz Pope <PapstDonB@Googlemail.com>
2015-06-02 14:25:15 +0200

    Adds CSV file output to commit message word count function

--- src/main/java/cr/processor/Main.java
+++ src/main/java/cr/processor/Main.java
@@ -2,6 +2,7 @@ package cr.processor;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.category.DefaultCategoryDataset;
@@ -9,10 +10,7 @@ import org.jfree.data.category.DefaultCategoryDataset;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
@@ -133,6 +131,7 @@ class Main {
 
     public static void wordRanksPlot(List<Author> authors) throws IOException {
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        Map<Author, Map<Integer, Integer>> occurrancesPerAuthor = new HashMap<>();
 
         for (Author author : authors) {
             Map<Integer, Integer> occurrances = new TreeMap<>(); // Number of words -> Messages with that number of words
@@ -145,12 +144,16 @@ class Main {
                 }
             }
 
+            occurrancesPerAuthor.put(author, occurrances);
+
             for (Integer words : occurrances.keySet()) {
                 dataset.addValue(occurrances.get(words), author.getName(), words);
             }
 
             //System.out.println("occurrances = " + occurrances);
         }
+
+        occurrancesPerAuthorToCSV(occurrancesPerAuthor);
 
         JFreeChart lineChart = ChartFactory.createLineChart3D("Commit message words per rank.", "ranks", "words", dataset);
         BufferedImage bufferedImage = lineChart.createBufferedImage(1028, 726);
@@ -161,5 +164,87 @@ class Main {
         BufferedImage image = ImageIO.read(in);
         File outputfile = new File("image.png");
         ImageIO.write(image, "png", outputfile);
+    }
+
+    public static void occurrancesPerAuthorToCSV(Map<Author, Map<Integer, Integer>> occurrancesPerAuthor) {
+        for (Author author : occurrancesPerAuthor.keySet()) {
+            System.out.println("occurrance[" + author.getName() + "] = " + occurrancesPerAuthor.get(author));
+        }
+
+        int maxWordCount = 0;
+        for (Author author : occurrancesPerAuthor.keySet()) {
+            for (Integer wordCount : occurrancesPerAuthor.get(author).keySet()) {
+                if (wordCount > maxWordCount) {
+                    maxWordCount = wordCount;
+                }
+            }
+        }
+        int numberOfAuthors = occurrancesPerAuthor.keySet().size();
+
+        System.out.println("maxWordCount = " + maxWordCount);
+
+        // Initialize the table
+        String[][] table = new String[maxWordCount + 1 + 1][];
+        for (int i = 0; i < maxWordCount + 1 + 1; i++) {
+            table[i] = new String[numberOfAuthors + 1];
+
+            for(int j = 0; j < numberOfAuthors + 1; j++) {
+                if (j == 0) {
+                    table[i][j] = i - 1 + "";
+                } else {
+                    table[i][j] = "0";
+                }
+            }
+        }
+
+        table[0][0] = "Rank";
+        int a = 1;
+        for (Author author : occurrancesPerAuthor.keySet()) {
+            table[0][a] = author.getName();
+
+            for (Integer wordCount : occurrancesPerAuthor.get(author).keySet()) {
+                table[wordCount + 1][a] = occurrancesPerAuthor.get(author).get(wordCount).toString();
+            }
+
+            a++;
+        }
+
+
+
+
+
+
+//        List<List<String>> table = new ArrayList<>(maxWordCount + 2);
+//        for (int i = 0; i < maxWordCount; i++) {
+//            int rowLength = occurrancesPerAuthor.keySet().size() + 1;
+//            ArrayList<String> row = new ArrayList<>();
+//            row.add()
+//            for (int j = 0; j < rowLength; j++) {
+//            }
+//            table.add(row);
+//        }
+//        table.get(0).add("Rank");
+//        for (Author author : occurrancesPerAuthor.keySet()) {
+//            table.get(0).add(author.getName());
+//        }
+//        for (Author author : occurrancesPerAuthor.keySet()) {
+//            for (Integer wordCount : occurrancesPerAuthor.get(author).keySet()) {
+//                table.get(wordCount)
+//            }
+//        }
+
+        try {
+            toCSV(table, "wordCounts.csv");
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void toCSV(String[][] table, String filename) throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter writer = new PrintWriter(filename, "UTF-8");
+        for (String[] row : table) {
+            writer.println(StringUtils.join(row, ","));
+        }
+        writer.close();
     }
 }
