c1e13223f66bd41b119239e22f092aefa5672914
Fritz Pope <PapstDonB@Googlemail.com>
2015-06-08 19:45:27 +0200

    Now actually comparing commits with their parent

--- src/main/java/cr/processor/Main.java
+++ src/main/java/cr/processor/Main.java
@@ -13,162 +13,168 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-	public static void main(String[] args) throws IOException {
-		// Read input from fetcher
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder input = new StringBuilder();
-		String line;
-		while ((line = br.readLine()) != null) {
-			input.append(line);
-		}
-		ObjectMapper mapper = new ObjectMapper();
-		FetchInput fetchInput = mapper.readValue(input.toString(), FetchInput.class);
-		List<AuthorData> authorDatas = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        // Read input from fetcher
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder input = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            input.append(line);
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        FetchInput fetchInput = mapper.readValue(input.toString(), FetchInput.class);
+        List<AuthorData> authorDatas = new ArrayList<>();
 
-		// Analyze authors
-		for (Author author : fetchInput.getAuthors()) {
-			if (author.getCommits().size() < 8) {
-				continue;
-			}
-			authorDatas.add(new AuthorData(author));
-		}
+        Runtime runtime = Runtime.getRuntime();
+        Process process = runtime.exec(new String[]{"carton", "exec", "./give-files", "-r", fetchInput.getLocal()});
+        BufferedReader diffToolReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        BufferedWriter diffToolWriter = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
 
-		//System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(authorDatas));
+        // Analyze authors
+        for (Author author : fetchInput.getAuthors()) {
+            if (author.getCommits().size() < 8) {
+                continue;
+            }
+            authorDatas.add(new AuthorData(author));
 
-		// Diff tool analysis
-		Runtime runtime = Runtime.getRuntime();
-		Process process = runtime.exec(new String[]{"carton", "exec", "./give-files", "-r", fetchInput.getLocal()});
-		BufferedReader diffToolReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-		BufferedWriter diffToolWriter = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
+            // Diff analysis
+            for (int c = 1; c < author.getCommits().size(); c++) {
+                Commit commit = author.getCommits().get(c);
+                if (commit.getParents().length > 1) {
+                    System.out.println(commit.getSha() + " has more than one parent, skipping");
+                    continue; // Skip merges
+                }
 
-		Author arthur = fetchInput.getAuthors().get(0);
-		for (int c = 1; c < arthur.getCommits().size(); c++) {
-			Commit prevCommit = arthur.getCommits().get(c - 1);
-			Commit commit = arthur.getCommits().get(c);
+                for (String fileName : commit.getDiff().keySet()) {
+                    if (!commit.getDiff().get(fileName).equals("M")) {
+                        continue; // Skip files that are created or deleted
+                    }
 
-			if (commit.getParents().length > 1) {
-//                System.out.println(commit.getSha() + " has more than one parent, skipping");
-				continue; // Skip merges
-			}
+                    String out = commit.getParents()[0]
+                            + "\0"
+                            + commit.getSha()
+                            + "\0"
+                            + fileName
+                            + "\n";
+                    System.out.printf("%n----%s%n", out);
+                    diffToolWriter.write(out);
+                    diffToolWriter.flush();
 
-			for (String fileName : commit.getDiff().keySet()) {
-				if (!commit.getDiff().get(fileName).equals("M")) {
-					continue; // Skip files that are created or deleted
-				}
+                    StringBuilder diffToolInput = new StringBuilder();
+                    try {
+                        while (!(line = diffToolReader.readLine()).contains("\0")) {
+                            diffToolInput.append(line);
+                        }
+                    } catch (NullPointerException e) {
+                        BufferedReader errorStream = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+                        String eline;
+                        while ((eline = errorStream.readLine()) != null) {
+                            System.err.println(eline);
+                        }
+                        throw e;
+                    }
+                    List<LineDiff> lineDiffs = mapper.readValue(diffToolInput.toString(), new TypeReference<List<LineDiff>>() {});
+                    System.out.println("lineDiffs = " + lineDiffs);
+                }
+            }
+        }
 
-				String out = prevCommit.getSha()
-						+ "\0"
-						+ commit.getSha()
-						+ "\0"
-						+ fileName
-						+ "\n";
-				System.out.printf("%n----%s%n", out);
-				diffToolWriter.write(out);
-				diffToolWriter.flush();
+        //System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(authorDatas));
 
-				StringBuilder diffToolInput = new StringBuilder();
-				while (!(line = diffToolReader.readLine()).contains("\0")) {
-					diffToolInput.append(line);
-				}
-				List<LineDiff> lineDiffs = mapper.readValue(diffToolInput.toString(), new TypeReference<List<LineDiff>>() {});
-				System.out.println("lineDiffs = " + lineDiffs);
-			}
-		}
+        diffToolReader.close();
+        diffToolWriter.close();
+    }
 
-		diffToolReader.close();
-		diffToolWriter.close();
-	}
+    public static void wordRanksPlot(List<Author> authors) throws IOException {
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        Map<Author, Map<Integer, Integer>> occurrancesPerAuthor = new HashMap<>();
 
-	public static void wordRanksPlot(List<Author> authors) throws IOException {
-		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-		Map<Author, Map<Integer, Integer>> occurrancesPerAuthor = new HashMap<>();
+        for (Author author : authors) {
+            Map<Integer, Integer> occurrances = new TreeMap<>(); // Number of words -> Messages with that number of words
+            for (Commit commit : author.getCommits()) {
+                int words = commit.getMessage().split("\\s+").length;
+                if (occurrances.get(words) != null) {
+                    occurrances.put(words, occurrances.get(words) + 1);
+                } else {
+                    occurrances.put(words, 1);
+                }
+            }
 
-		for (Author author : authors) {
-			Map<Integer, Integer> occurrances = new TreeMap<>(); // Number of words -> Messages with that number of words
-			for (Commit commit : author.getCommits()) {
-				int words = commit.getMessage().split("\\s+").length;
-				if (occurrances.get(words) != null) {
-					occurrances.put(words, occurrances.get(words) + 1);
-				} else {
-					occurrances.put(words, 1);
-				}
-			}
+            occurrancesPerAuthor.put(author, occurrances);
 
-			occurrancesPerAuthor.put(author, occurrances);
+            for (Integer words : occurrances.keySet()) {
+                dataset.addValue(occurrances.get(words), author.getName(), words);
+            }
 
-			for (Integer words : occurrances.keySet()) {
-				dataset.addValue(occurrances.get(words), author.getName(), words);
-			}
+            //System.out.println("occurrances = " + occurrances);
+        }
 
-			//System.out.println("occurrances = " + occurrances);
-		}
+        occurrancesPerAuthorToCSV(occurrancesPerAuthor);
 
-		occurrancesPerAuthorToCSV(occurrancesPerAuthor);
+        JFreeChart lineChart = ChartFactory.createLineChart3D("Commit message words per rank.", "ranks", "words", dataset);
+        BufferedImage bufferedImage = lineChart.createBufferedImage(1028, 726);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ImageIO.write(bufferedImage, "png", byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+        InputStream in = new ByteArrayInputStream(byteArray);
+        BufferedImage image = ImageIO.read(in);
+        File outputfile = new File("image.png");
+        ImageIO.write(image, "png", outputfile);
+    }
 
-		JFreeChart lineChart = ChartFactory.createLineChart3D("Commit message words per rank.", "ranks", "words", dataset);
-		BufferedImage bufferedImage = lineChart.createBufferedImage(1028, 726);
-		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-		ImageIO.write(bufferedImage, "png", byteArrayOutputStream);
-		byte[] byteArray = byteArrayOutputStream.toByteArray();
-		InputStream in = new ByteArrayInputStream(byteArray);
-		BufferedImage image = ImageIO.read(in);
-		File outputfile = new File("image.png");
-		ImageIO.write(image, "png", outputfile);
-	}
-
-	public static void occurrancesPerAuthorToCSV(Map<Author, Map<Integer, Integer>> occurrancesPerAuthor) {
+    public static void occurrancesPerAuthorToCSV(Map<Author, Map<Integer, Integer>> occurrancesPerAuthor) {
 //        for (Author author : occurrancesPerAuthor.keySet()) {
 //            System.out.println("occurrance[" + author.getName() + "] = " + occurrancesPerAuthor.get(author));
 //        }
 
-		int maxWordCount = 0;
-		for (Author author : occurrancesPerAuthor.keySet()) {
-			for (Integer wordCount : occurrancesPerAuthor.get(author).keySet()) {
-				if (wordCount > maxWordCount) {
-					maxWordCount = wordCount;
-				}
-			}
-		}
-		int numberOfAuthors = occurrancesPerAuthor.keySet().size();
+        int maxWordCount = 0;
+        for (Author author : occurrancesPerAuthor.keySet()) {
+            for (Integer wordCount : occurrancesPerAuthor.get(author).keySet()) {
+                if (wordCount > maxWordCount) {
+                    maxWordCount = wordCount;
+                }
+            }
+        }
+        int numberOfAuthors = occurrancesPerAuthor.keySet().size();
 
-		//System.out.println("maxWordCount = " + maxWordCount);
+        //System.out.println("maxWordCount = " + maxWordCount);
 
-		// Initialize the table
-		String[][] table = new String[maxWordCount + 1 + 1][];
-		for (int i = 0; i < maxWordCount + 1 + 1; i++) {
-			table[i] = new String[numberOfAuthors + 1];
+        // Initialize the table
+        String[][] table = new String[maxWordCount + 1 + 1][];
+        for (int i = 0; i < maxWordCount + 1 + 1; i++) {
+            table[i] = new String[numberOfAuthors + 1];
 
-			for (int j = 0; j < numberOfAuthors + 1; j++) {
-				if (j == 0) {
-					table[i][j] = i - 1 + "";
-				} else {
-					table[i][j] = "0";
-				}
-			}
-		}
+            for (int j = 0; j < numberOfAuthors + 1; j++) {
+                if (j == 0) {
+                    table[i][j] = i - 1 + "";
+                } else {
+                    table[i][j] = "0";
+                }
+            }
+        }
 
-		table[0][0] = "Rank";
-		int a = 1;
-		for (Author author : occurrancesPerAuthor.keySet()) {
-			table[0][a] = author.getName();
-			for (Integer wordCount : occurrancesPerAuthor.get(author).keySet()) {
-				table[wordCount + 1][a] = occurrancesPerAuthor.get(author).get(wordCount).toString();
-			}
-			a++;
-		}
+        table[0][0] = "Rank";
+        int a = 1;
+        for (Author author : occurrancesPerAuthor.keySet()) {
+            table[0][a] = author.getName();
+            for (Integer wordCount : occurrancesPerAuthor.get(author).keySet()) {
+                table[wordCount + 1][a] = occurrancesPerAuthor.get(author).get(wordCount).toString();
+            }
+            a++;
+        }
 
-		try {
-			toCSV(table, "wordCounts.csv");
-		} catch (FileNotFoundException | UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-	}
+        try {
+            toCSV(table, "wordCounts.csv");
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static void toCSV(String[][] table, String filename) throws FileNotFoundException, UnsupportedEncodingException {
-		PrintWriter writer = new PrintWriter(filename, "UTF-8");
-		for (String[] row : table) {
-			writer.println(StringUtils.join(row, ","));
-		}
-		writer.close();
-	}
+    public static void toCSV(String[][] table, String filename) throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter writer = new PrintWriter(filename, "UTF-8");
+        for (String[] row : table) {
+            writer.println(StringUtils.join(row, ","));
+        }
+        writer.close();
+    }
 }
