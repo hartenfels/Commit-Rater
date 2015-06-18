27f5b3406e15cc6b415932749299784e616e998d
Carsten Hartenfels <hartenfels@uni-koblenz.de>
2015-06-18 04:40:47 +0200

    Split rate into rate and rate_commit
    Then rate_commit can be called using each_commit.

--- CommitRater.pm
+++ CommitRater.pm
@@ -13,7 +13,14 @@ sub rate
 {
     my ($self) = @_;
     $self->repo->update;
-    $self->repo->each_commit(sub { print "$_->{sha}\n" });
+    $self->repo->each_commit(sub { $self->rate_commit($_) });
+}
+
+
+sub rate_commit
+{
+    my ($self, $commit) = @_;
+    print "$commit->{message}\n";
 }
 
 
