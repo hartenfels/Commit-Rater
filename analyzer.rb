1621cc6ae42b06e78e7232f7d6176714b0fe782c
turbopope <PapstDonB@Googlemail.com>
2015-08-26 02:12:39 +0200

    %w

--- analyzer.rb
+++ analyzer.rb
@@ -25,21 +25,7 @@ def countCommits(resultTriples)
   return resultTriples['subject_limit']['pass'] + resultTriples['subject_limit']['fail']
 end
 
-criteria = [
-  "body_limit",
-  "body_used",
-  "capitalize_subject",
-  "empty_second_line",
-  "imperative_subject",
-  "no_bulk_change",
-  "no_duplicate",
-  "no_long_message",
-  "no_misspelling",
-  "no_period_subject",
-  "no_short_message",
-  "no_vulgarity",
-  "subject_limit"
-]
+criteria = %w[body_limit body_used capitalize_subject empty_second_line imperative_subject no_bulk_change no_duplicate no_long_message no_misspelling no_period_subject no_short_message no_vulgarity subject_limit]
 
 result = Hash.new
 result['authors'] = Hash.new
