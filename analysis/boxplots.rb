9aa73f12f71440f3b1968f275f223db8266f50ce
turbopope <PapstDonB@Googlemail.com>
2015-08-26 19:17:44 +0200

    Add manual median calculation to boxplot script for testing

--- analysis/boxplots.rb
+++ analysis/boxplots.rb
@@ -4,16 +4,11 @@ require 'json'
 require 'pp'
 require 'statsample'
 
-# ss_analysis(Statsample::Graph::Boxplot) do
-#   a = [0.0, 0.1, 0.3, 0.5, 0.7, 0.99].to_numeric
-#   b = [0.0, 0.1, 0.125, 0.15, 0.1, 0.2, 0.99, 0.05].to_numeric
-#   c = [0.5, 0.51, 0.52, 0.53, 0.54].to_numeric
-#   d = [1, 1].to_numeric
-#
-#   boxplot(:vectors=>[d], :width=>600, :height=>600, :groups=>[0,1,2])
-# end
-# Statsample::Analysis.run
-
+def median(array)
+  sorted = array.sort
+  len = sorted.length
+  return (sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0
+end
 
 criteria = %w[body_limit body_used capitalize_subject empty_second_line imperative_subject no_bulk_change no_duplicate no_long_message no_misspelling no_period_subject no_short_message no_vulgarity subject_limit]
 
@@ -36,7 +31,8 @@ criteria.each do |criterion|
         repo_criterion_rates.push(rate)
       end
       puts repo
-      puts repo_criterion_rates.inject{|sum, el| sum + el}.to_f / repo_criterion_rates.size
+      puts median(repo_criterion_rates)
+      #puts repo_criterion_rates.inject{|sum, el| sum + el}.to_f / repo_criterion_rates.size
       boxplot_arrays.push(repo_criterion_rates.to_numeric)
     end
     svg = Statsample::Graph::Boxplot.new(
